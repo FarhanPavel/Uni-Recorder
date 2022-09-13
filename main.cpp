845138040849ec811c7fe3fb447b44e6ebfc346e
Farhan Pavel <107743709+FarhanPavel@users.noreply.github.com>
2022-09-14 00:59:50 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+
+   cout<<endl;
+   cout<<"\t\t\t       "<<"%zpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzp%";
+   cout<<endl<<"\t\t\t       p\t\t\t\t\t\t    z"<<endl<<"\t\t\t       z\t\t\t\t\t\t    p";
+   cout<<""<<endl;//project name
+   cout<<"\t\t\t       p\t"<<" Student Record Management System"<<"\t    z"<<endl;
+   cout<<"\t\t\t       z\t"<<"\t\t\t\t\t    p"<<"\n";
+   cout<<"\t\t\t       p\t"<<"\t\t\t\t\t    z"<<"\n";
+   cout<<"\t\t\t       z\t"<<"   Developed By: Pavel && Zunaid\t    p"<<endl<<"\t\t\t       p\t\t\t\t\t\t    z"<<endl;
+   cout<<"\t\t\t       z\t\t    "<<"IIT-12\t\t\t    p"<<endl<<"\t\t\t       p\t\t\t\t\t\t    z"<<endl;
+   cout<<"\t\t\t       z\t     "<<"Jahangirnagar University\t\t    p"<<endl<<"\t\t\t       p\t   \t\t\t\t\t    z"<<endl;
+   cout<<"\t\t\t       z\t     \t\t\t\t\t    p"<<endl<<"\t\t\t       p\t     \t\t\t\t\t    z"<<endl;
+   cout<<"\t\t\t       "<<"%pzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpzpz%";
+
+    getch();
+}
